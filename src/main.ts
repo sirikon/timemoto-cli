a754b445b5ea937c38a79d51dcbba2a8cbf72051
Carlos Fdez. Llamas <hello@sirikon.me>
2021-08-30 16:06:55 +0200

    Execute with current week by default

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,20 @@ import * as c from 'ansi-colors'
 import * as timemoto from './timemoto/index'
 import { formatDate, formatDuration } from './format'
 import { ensureConfig } from './config/config';
+import moment from 'moment';
+
+type DateRange = {
+    from: Date,
+    to: Date
+}
 
 async function main() {
     const config = await ensureConfig();
+    const currentWeek = getCurrentWeek();
 
-    const auth = await timemoto.login(config.credentials);
-    const days = await timemoto.getDays(
-        auth,
-        new Date(Date.UTC(2020, 7, 10)),
-        new Date(Date.UTC(2020, 7, 24)));
-        
+    const session = await timemoto.login(config.credentials);
+    const days = await getDays(session, currentWeek);
+
     days.forEach(day => {
         if ([6, 0].indexOf(day.date.getDay()) >= 0) {
             console.log(c.grey(`${formatDate(day.date)} -> Weekend`));
@@ -20,6 +24,22 @@ async function main() {
         }
         console.log(`${formatDate(day.date)} -> ${formatDuration(day.duration)}`);
     });
+}
+
+async function getDays(session: timemoto.TimemotoSession, dayRange: DateRange) {
+    return await timemoto.getDays(session, dayRange.from, dayRange.to);
+}
+
+function getCurrentWeek(): DateRange {
+    const now = new Date();
+    const today = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
+
+    const firstDay = moment(today).add(- ((today.getUTCDay() - 1 + 7) % 7), 'day').toDate();
+    
+    return {
+        from: firstDay,
+        to: moment(firstDay).add(6, 'day').toDate()
+    }
 }
 
 main().then(() => {}, (err) => console.log(err));
